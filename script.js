b3d0cb728e8f14fd96abca4e51ab8c32df70efb5
Jef <120083186+koldtea@users.noreply.github.com>
2023-02-22 14:31:34 +0800

    Add basic file structure with starting pseudocode

---
+++ script.js
@@ -0,0 +1,17 @@
+// let computer return random variable between rock, paper, scissors
+
+
+
+// play a single round of RPS that receives input from player and computer
+
+
+
+// return string that declares winner of the round
+
+
+
+// create RPS game that loops for 5 rounds and keeps score
+
+
+
+// return string that declares winner/loser of 5
