e6370b6a655dcfe3c9ad6de2f49c83a34169e1fc
Jef <120083186+koldtea@users.noreply.github.com>
2023-02-22 20:03:23 +0800

    Release RPS script v1

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+
+// computer generates random variable
 // let computer return random variable between rock, paper, scissors
+const choicearray = ["rock", "paper", "scissors"];
+
 function getComputerChoice(choicearray) {
   
   const randomChoice = Math.floor(Math.random() * choicearray.length);
@@ -6,45 +10,40 @@ function getComputerChoice(choicearray) {
   
   return choice;
 }
-const choicearray = ["rock", "paper", "scissors"];
 const result = getComputerChoice(choicearray);
-// console.log(result);
 
-// play inputs a string (rock, paper, scissors)
-const playerSelect = prompt ("Rock, paper, or scissors?").toLowerCase();
-console.log(playerSelect);
-// computer generates random variable
-const computerSelect = getComputerChoice(choicearray);
-console.log(computerSelect);
+let computerSelect = getComputerChoice(choicearray);
 
-// play a single round of RPS that receives input from player and computer
+// declare score variables to store player and computer score
+let pScore = 0;
+let cScore = 0;
 
 function playRound(playerSelect, computerSelect) {
-  if (playerSelect == "rock" && computerSelect == "paper") {
+  if (playerSelect == computerSelect) {
+    return "It's a draw!";
+  }
+  else if (playerSelect == "rock" && computerSelect == "paper") {
+    cScore =+1;
     return "You lose! Paper beats Rock.";
   }
   else if (playerSelect == "rock" && computerSelect == "scissors") {
+    pScore =+1;
     return "You won! Rock beats Scissors.";
   }
-  else if (playerSelect == "rock" && computerSelect == "rock") {
-    return "It's a draw!";
-  }
   else if (playerSelect == "scissors" && computerSelect == "paper") {
+    pScore =+1;
     return "You won! Scissors beats Paper.";
   }
-  else if (playerSelect == "scissors" && computerSelect == "scissors") {
-    return "It's a draw!";
-  }
   else if (playerSelect == "scissors" && computerSelect == "rock") {
+    cScore =+1;
     return "You lost! Rock beats Scissors.";
   }
-  else if (playerSelect == "paper" && computerSelect == "paper") {
-    return "It's a draw!";
-  }
   else if (playerSelect == "paper" && computerSelect == "scissors") {
+    cScore =+1;
     return "You lose! Scissors beats Paper.";
   }
   else if (playerSelect == "paper" && computerSelect == "rock") {
+    pScore =+1;
     return "You won! Paper beats Rock.";
   }
   else {
@@ -52,9 +51,28 @@ function playRound(playerSelect, computerSelect) {
   }
 }
 
-alert(playRound(playerSelect, computerSelect));
-console.log(playRound(playerSelect, computerSelect));
+// play inputs a string (rock, paper, scissors)
+// loop RPS five times
+// return string that declares winner/loser of 5 rounds
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playerSelect = prompt ("Rock, Paper, or Scissors?").toLowerCase();
+    computerSelect = getComputerChoice(choicearray);
+    
+    console.log("Player: " + playerSelect + " Computer: " + computerSelect);
+    alert("Player: " + playerSelect + " Computer: " + computerSelect);
+    
+    console.log(playRound(playerSelect, computerSelect));
+  }
+  if (pScore > cScore) {
+    return "Your total score was " + pScore + ", you won the game!";
+  }
+  if (pScore == cScore) {
+    return "The game's a tie!";
+  }
+  else {
+    return "Your total score was " + pScore + ", you lost the game!"
+  }
+}
 
-// create RPS game that loops for 5 rounds and keeps score
-
-// return string that declares winner/loser of 5
+console.log(game());
